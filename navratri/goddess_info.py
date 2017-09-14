394b1a2455dfb3c1efe1651a9d9cfb2f4685d31c
Karthik Prakash <karthik.prakash@happiestminds.com>
2017-09-14 14:09:43 +0530

    Added Audio support for all Durgas

--- navratri/goddess_info.py
+++ navratri/goddess_info.py
@@ -1,9 +1,23 @@
-durga_names_to_images = {"SHAILAPUTRI": "shailaputri_durga_1.jpg",
-                         "BRAHMACHARINI": "brahmacharini_durga_2.jpg",
-                         "CHANDRAGHANTA": "chandraghanta_durga_3.jpg",
-                         "KUSHMANDA": "kushmanda_durga4.jpg",
-                         "SKANDA MATA": "skandamata_durga_5.jpg",
-                         "KATYAYANI": "katyayani_durga_6.jpg",
-                         "KALARATRI": "kalaratri_durga_7.jpg",
-                         "MAHAGAURI": "mahagauri_durga_8.jpg",
-                         "SIDDHIDATRI": "siddhidatri_durga_9.jpg"}
+durga_names = {1: "SHAILAPUTRI", 2: "BRAHMACHARINI", 3: "CHANDRAGHANTA",
+               4: "KUSHMANDA", 5: "SKANDA MATA", 6: "KATYAYANI",
+               7: "KALARATRI", 8: "MAHAGAURI", 9: "SIDDHIDATRI"}
+
+durga_names_to_images = {1: "shailaputri_durga_1.jpg",
+                         2: "brahmacharini_durga_2.jpg",
+                         3: "chandraghanta_durga_3.jpg",
+                         4: "kushmanda_durga4.jpg",
+                         5: "skandamata_durga_5.jpg",
+                         6: "katyayani_durga_6.jpg",
+                         7: "kalaratri_durga_7.jpg",
+                         8: "mahagauri_durga_8.jpg",
+                         9: "siddhidatri_durga_9.jpg"}
+
+durga_names_to_music = {1: "durga1.mp3",
+                        2: "durga2.mp3",
+                        3: "durga3.mp3",
+                        4: "durga4.mp3",
+                        5: "durga5.mp3",
+                        6: "durga6.mp3",
+                        7: "durga7.mp3",
+                        8: "durga8.mp3",
+                        9: "durga9.mp3"}
--- navratri/test_flask.py
+++ navratri/test_flask.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template
+import goddess_info
 
 app = Flask(__name__, static_url_path="/static")
-@app.route("/")
-def hello():
-    return "Hello World"
+
 
 @app.route("/DurgaInterface")
 def durga_interface():
-    return render_template("durga_interface.html")
+    template_data = {'goddess_names': goddess_info.durga_names,
+                     'goddess_images': goddess_info.durga_names_to_images}
+
+    return render_template("durga_interface.html", **template_data)
+
 
 @app.route("/DurgaInterface/<goddess_name>/")
-def play_song(goddess_name):
-    return render_template("playing_song.html")
+def perform(goddess_name):
+    # Get Name of the Music File
+    for key, value in goddess_info.durga_names.iteritems():
+        if value.upper() == goddess_name.upper():
+            music_file = goddess_info.durga_names_to_music[key]
+
+    template_data = {'goddess_name': goddess_name.upper(), "music_file": music_file}
+    return render_template("playing_song.html", **template_data)
+
 
 if __name__ == "__main__":
     app.run(host="192.168.56.1", port=5000, debug=True)
